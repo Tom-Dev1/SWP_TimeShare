2c46f00fb0fc6115c53d39c5e216a09d8ee38c1c
Quang Truong <54901314+MeoKool@users.noreply.github.com>
2024-02-27 17:24:36 +0700

    Revert "detail-realestate"

--- src/components/Admin/feedback/dashBoardFeedBack.jsx
+++ src/components/Admin/feedback/dashBoardFeedBack.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Box,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    TablePagination,
-    Select,
-    MenuItem,
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
@@ -18,166 +18,174 @@ import StarRatings from "react-star-ratings";
 import { GetAllFeedback, GetUserByID } from "../../API/APIConfigure";
 
 const Dashboard = () => {
-    const [feedback, setFeedback] = useState([]);
-    const [userDetails, setUserDetails] = useState({});
-    const [selectedStatusFilter, setSelectedStatusFilter] = useState("all");
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-    const [page, setPage] = useState(0);
+  const [feedback, setFeedback] = useState([]);
+  const [userDetails, setUserDetails] = useState({});
+  const [selectedStatusFilter, setSelectedStatusFilter] = useState("all");
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
 
-    useEffect(() => {
-        const fetchFeedback = async () => {
-            try {
-                const response = await GetAllFeedback();
-                setFeedback(Array.isArray(response) ? response : []);
-            } catch (err) {
-                toast.error("Failed to fetch feedback");
-                console.error(err);
-            }
-        };
+  useEffect(() => {
+    const fetchFeedback = async () => {
+      try {
+        const response = await GetAllFeedback();
+        setFeedback(Array.isArray(response) ? response : []);
+      } catch (err) {
+        toast.error("Failed to fetch feedback");
+        console.error(err);
+      }
+    };
 
-        fetchFeedback();
-    }, []);
+    fetchFeedback();
+  }, []);
 
-    useEffect(() => {
-        const userIds = feedback.map((item) => item.memberId);
-        const uniqueUserIds = Array.from(new Set(userIds));
+  useEffect(() => {
+    const userIds = feedback.map((item) => item.memberId);
+    const uniqueUserIds = Array.from(new Set(userIds));
 
-        const fetchUserDetails = async () => {
-            uniqueUserIds.forEach(async (id) => {
-                try {
-                    const userData = await GetUserByID(id);
-                    setUserDetails((prevDetails) => ({
-                        ...prevDetails,
-                        [id]: userData.username,
-                    }));
-                } catch (error) {
-                    console.error("Failed to fetch user details", error);
-                    toast.error(`Failed to fetch user details for ID: ${id}`);
-                }
-            });
-        };
-
-        if (uniqueUserIds.length > 0) {
-            fetchUserDetails();
+    const fetchUserDetails = async () => {
+      uniqueUserIds.forEach(async (id) => {
+        try {
+          const userData = await GetUserByID(id);
+          setUserDetails((prevDetails) => ({
+            ...prevDetails,
+            [id]: userData.username,
+          }));
+        } catch (error) {
+          console.error("Failed to fetch user details", error);
+          toast.error(`Failed to fetch user details for ID: ${id}`);
         }
-    }, [feedback]);
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+      });
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+    if (uniqueUserIds.length > 0) {
+      fetchUserDetails();
+    }
+  }, [feedback]);
 
-    const filteredFeedback = feedback.filter((item) => {
-        return selectedStatusFilter === "all" || item.rate.toString() === selectedStatusFilter;
-    });
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const slicedFeedback = filteredFeedback.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
+  const filteredFeedback = feedback.filter((item) => {
     return (
-        <Box sx={{ display: "flex" }}>
-            <Box component="main" sx={{ flexGrow: 1, p: 5 }}>
-                <Select
-                    value={selectedStatusFilter}
-                    onChange={(e) => setSelectedStatusFilter(e.target.value)}
-                    style={{ marginTop: "30px", marginBottom: "20px" }}
-                >
-                    <MenuItem value="all">Tất cả</MenuItem>
-                    {[5, 4, 3, 2, 1].map((rating) => (
-                        <MenuItem key={rating} value={rating.toString()}>
-                            {rating} sao
-                        </MenuItem>
-                    ))}
-                </Select>
-                <TableContainer component={Paper}>
-                    <h2
-                        style={{
-                            textAlign: "center",
-                            color: "#205295",
-                            fontSize: "40px",
-                            marginTop: "20px",
-                            marginBottom: "20px",
-                            fontFamily: "Arial, sans-serif",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Đánh giá
-                    </h2>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell
-                                    style={{
-                                        fontSize: "20px",
-                                        fontFamily: "Arial, sans-serif",
-                                    }}
-                                    align="center"
-                                >
-                                    Tên tài khoản
-                                </TableCell>
-                                <TableCell
-                                    style={{
-                                        fontSize: "20px",
-                                        fontFamily: "Arial, sans-serif",
-                                    }}
-                                    align="center"
-                                >
-                                    Bất động sản
-                                </TableCell>
-                                <TableCell
-                                    style={{
-                                        fontSize: "20px",
-                                        fontFamily: "Arial, sans-serif",
-                                    }}
-                                    align="center"
-                                >
-                                    Feedback
-                                </TableCell>
-                                <TableCell
-                                    style={{
-                                        fontSize: "20px",
-                                        fontFamily: "Arial, sans-serif",
-                                    }}
-                                    align="center"
-                                >
-                                    Bình chọn
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {slicedFeedback.map((item) => (
-                                <TableRow key={item.id}>
-                                    <TableCell align="center">{userDetails[item.memberId] || item.memberId}</TableCell>
-                                    <TableCell align="center">{item.text}</TableCell>
-                                    <TableCell align="center">{item.text}</TableCell>
-                                    <TableCell align="center">
-                                        <StarRatings
-                                            rating={item.rate}
-                                            starDimension="20px"
-                                            starSpacing="2px"
-                                            starRatedColor="orange"
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                    <TablePagination
-                        rowsPerPageOptions={[5, 10, 25]}
-                        component="div"
-                        count={filteredFeedback.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                </TableContainer>
-            </Box>
-        </Box>
+      selectedStatusFilter === "all" ||
+      item.rate.toString() === selectedStatusFilter
     );
+  });
+
+  const slicedFeedback = filteredFeedback.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 5 }}>
+        <Select
+          value={selectedStatusFilter}
+          onChange={(e) => setSelectedStatusFilter(e.target.value)}
+          style={{ marginTop: "30px", marginBottom: "20px" }}
+        >
+          <MenuItem value="all">Tất cả</MenuItem>
+          {[5, 4, 3, 2, 1].map((rating) => (
+            <MenuItem key={rating} value={rating.toString()}>
+              {rating} sao
+            </MenuItem>
+          ))}
+        </Select>
+        <TableContainer component={Paper}>
+          <h2
+            style={{
+              textAlign: "center",
+              color: "#205295",
+              fontSize: "40px",
+              marginTop: "20px",
+              marginBottom: "20px",
+              fontFamily: "Arial, sans-serif",
+              fontWeight: "bold",
+            }}
+          >
+            Đánh giá
+          </h2>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  style={{
+                    fontSize: "20px",
+                    fontFamily: "Arial, sans-serif",
+                  }}
+                  align="center"
+                >
+                  Tên tài khoản
+                </TableCell>
+                <TableCell
+                  style={{
+                    fontSize: "20px",
+                    fontFamily: "Arial, sans-serif",
+                  }}
+                  align="center"
+                >
+                  Bất động sản
+                </TableCell>
+                <TableCell
+                  style={{
+                    fontSize: "20px",
+                    fontFamily: "Arial, sans-serif",
+                  }}
+                  align="center"
+                >
+                  Feedback
+                </TableCell>
+                <TableCell
+                  style={{
+                    fontSize: "20px",
+                    fontFamily: "Arial, sans-serif",
+                  }}
+                  align="center"
+                >
+                  Bình chọn
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {slicedFeedback.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell align="center">
+                    {userDetails[item.memberId] || item.memberId}
+                  </TableCell>
+                  <TableCell align="center">{item.text}</TableCell>
+                  <TableCell align="center">{item.text}</TableCell>
+                  <TableCell align="center">
+                    <StarRatings
+                      rating={item.rate}
+                      starDimension="20px"
+                      starSpacing="2px"
+                      starRatedColor="orange"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={filteredFeedback.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </TableContainer>
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
