89aa46e47c38bfcc0d64eaef424978ba5d156526
Quang Truong <truongsyquang1202@gmail.com>
2024-02-27 22:00:03 +0700

    update fill status - fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
-import './footer.css';
+import "./footer.css";
 
 function Footer() {
   return (
     <div className="Footer">
       <div className="FLists">
-        <ui className="FList">
+        <ul className="FList">
           <li className="fListItem">Các quốc gia</li>
           <li className="fListItem">Khu vực</li>
           <li className="fListItem">Thành Phố</li>
@@ -12,30 +12,30 @@ function Footer() {
           <li className="fListItem">Sân bay</li>
           <li className="fListItem">Khách sạn</li>
           <li className="fListItem">Địa điểm quan tâm</li>
-        </ui>
-        <ui className="FList">
+        </ul>
+        <ul className="FList">
           <li className="fListItem">Nhà</li>
           <li className="fListItem">Căn Hộ</li>
           <li className="fListItem">Resort</li>
           <li className="fListItem">Biệt Thự</li>
           <li className="fListItem">Nhà trọ</li>
           <li className="fListItem">Nhà nghỉ B&B</li>
-        </ui>
-        <ui className="FList">
+        </ul>
+        <ul className="FList">
           <li className="fListItem">Những khu nghỉ độc đáo</li>
           <li className="fListItem">Tất cả các điểm đến</li>
           <li className="fListItem">Tất cả các điểm đến có chuyển bay</li>
           <li className="fListItem">Tất cả các địa điểm cho thuê xe</li>
           <li className="fListItem">Khám Phá</li>
           <li className="fListItem">Đánh giá của khách hàng</li>
-        </ui>
-        <ui className="FList">
+        </ul>
+        <ul className="FList">
           <li className="fListItem">Cho thuê xe hơi</li>
           <li className="fListItem">Tìm chuyến bay</li>
           <li className="fListItem">Đặt nhà hàng</li>
           <li className="fListItem">Đại Lý Du Lịch</li>
-        </ui>
-        <ui className="FList">
+        </ul>
+        <ul className="FList">
           <li className="fListItem">Dịch vụ khách hàng</li>
           <li className="fListItem">Trợ giúp đối tác</li>
           <li className="fListItem">Careers</li>
@@ -44,9 +44,11 @@ function Footer() {
           <li className="fListItem">Trung tâm thông tin bảo mật</li>
           <li className="fListItem">Điều khoản và điều kiện</li>
           <li className="fListItem">Liên hệ công ty</li>
-        </ui>
+        </ul>
       </div>
-      <div className="text">Bản quyền © 1996–2024 Booking.com™. Bảo lưu mọi quyền.</div>
+      <div className="text">
+        Bản quyền © 1996–2024 Booking.com™. Bảo lưu mọi quyền.
+      </div>
     </div>
   );
 }
--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -9,37 +9,39 @@ const SearchItem = ({ searchResult }) => {
 
   return (
     <div className="search-result-content search-result-list search-result-card-list">
-      {searchResult.map((item) => {
-        const photoUrls = item.photo ? item.photo.split(",") : [];
-        return (
-          <div key={item.id} className="result-card-wrapper small-12">
-            <Link to={`/hotels/${item.id}`}>
-              <div className="result-card">
-                <img
-                  className="result-card--photo"
-                  src={BASE_URL + photoUrls[0]}
-                  alt={item.name}
-                />
-                <div className="result-card--info">
-                  <h2 className="title-h2">
-                    <p>{item.name}</p>
-                  </h2>
-                  <div className="result-card--info_location">
-                    {item.location}
-                  </div>
+      {searchResult
+        .filter((item) => item.status === "1")
+        .map((item) => {
+          const photoUrls = item.photo ? item.photo.split(",") : [];
+          return (
+            <div key={item.id} className="result-card-wrapper small-12">
+              <Link to={`/hotels/${item.id}`}>
+                <div className="result-card">
+                  <img
+                    className="result-card--photo"
+                    src={BASE_URL + photoUrls[0]}
+                    alt={item.name}
+                  />
+                  <div className="result-card--info">
+                    <h2 className="title-h2">
+                      <p>{item.name}</p>
+                    </h2>
+                    <div className="result-card--info_location">
+                      {item.location}
+                    </div>
 
-                  <Rating name="simple-controlled" value={value} readOnly />
-                </div>
-                <div className="result-card--cta">
-                  <div className="result-card--cta_item">
-                    <span>{item.price}/Night</span>
+                    <Rating name="simple-controlled" value={value} readOnly />
+                  </div>
+                  <div className="result-card--cta">
+                    <div className="result-card--cta_item">
+                      <span>{item.price}/Night</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        );
-      })}
+              </Link>
+            </div>
+          );
+        })}
     </div>
   );
 };
