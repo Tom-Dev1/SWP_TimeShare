560931394957077fb47dd662fd65e8e1ed82c382
hpdzz <tranlehuuphuoc22502@gmail.com>
2024-02-28 21:22:26 +0700

    create-booking

--- src/components/API/APIConfigure.jsx
+++ src/components/API/APIConfigure.jsx
@@ -1,70 +1,73 @@
-import instance, { customAxios } from '../setUp/axios';
+import instance, { customAxios } from "../setUp/axios";
 
 export const BASE_URL = "http://meokool-001-site1.ltempurl.com/";
 
 const SignInAccount = (userData) => {
-  return instance.post(`api/Accounts/Signin?${userData}`);
+    return instance.post(`api/Accounts/Signin?${userData}`);
 };
 
 const SignUpAccount = (userData) => {
-  return instance.post(`api/Accounts/SignUpUser`, userData);
+    return instance.post(`api/Accounts/SignUpUser`, userData);
 };
 const GetAllAccounts = () => {
-  return customAxios.get(`api/Accounts/GetAll`);
+    return customAxios.get(`api/Accounts/GetAll`);
 };
 
 const DeleteAccount = (userID) => {
-  return customAxios.delete(`api/Accounts/DeleteAccount?id=${userID}`);
+    return customAxios.delete(`api/Accounts/DeleteAccount?id=${userID}`);
 };
 
 const UpdateStatus = (userID, newStatus) => {
-  return customAxios.put(`api/Accounts/UpdateAccountStatus?id=${userID}`, {
-    status: newStatus,
-  });
+    return customAxios.put(`api/Accounts/UpdateAccountStatus?id=${userID}`, {
+        status: newStatus,
+    });
 };
 export const UpdateAccountByID = (userID, newUserData) => {
-  return customAxios.put(`api/Accounts/UpdateAccount?id=${userID}`, {
-    ...newUserData,
-  });
+    return customAxios.put(`api/Accounts/UpdateAccount?id=${userID}`, {
+        ...newUserData,
+    });
 };
 export const UpdatePasswordByID = (userID, newPassword) => {
-  return customAxios.put(`api/Accounts/UpdateAccount?id=${userID}`, {
-    password: newPassword,
-  });
+    return customAxios.put(`api/Accounts/UpdateAccount?id=${userID}`, {
+        password: newPassword,
+    });
 };
 const GetAllBookings = () => {
-  return customAxios.get(`api/Bookings/GetAll`);
+    return customAxios.get(`api/Bookings/GetAll`);
 };
 const GetAllVoucher = () => {
-  return customAxios.get(`api/Vouchers/GetAll`);
+    return customAxios.get(`api/Vouchers/GetAll`);
 };
 const GetAllFeedback = () => {
-  return customAxios.get(`api/Feedbacks/GetAll`);
+    return customAxios.get(`api/Feedbacks/GetAll`);
 };
 const GetAllRealestates = () => {
-  return customAxios.get(`api/Realestates/GetAll`);
+    return customAxios.get(`api/Realestates/GetAll`);
 };
 const GetUserByID = (userID) => {
-  return customAxios.get(`api/Accounts/GetbyID?id=${userID}`);
+    return customAxios.get(`api/Accounts/GetbyID?id=${userID}`);
 };
 const CreateVouchers = (voucherData) => {
-  return instance.post(`api/Vouchers/Createvoucher`, voucherData);
+    return instance.post(`api/Vouchers/Createvoucher`, voucherData);
 };
 export const GetbyRealestateID = (realetatesID) => {
-  return customAxios.get(
-    `api/Realestates/GetbyRealestateID?id=${realetatesID}`
-  );
+    return customAxios.get(`api/Realestates/GetbyRealestateID?id=${realetatesID}`);
+};
+
+const CreateBooking = (data) => {
+    return customAxios.post(`api/Bookings/Createbooking`, data);
 };
 export {
-  SignInAccount,
-  SignUpAccount,
-  GetAllAccounts,
-  DeleteAccount,
-  UpdateStatus,
-  GetAllBookings,
-  GetAllVoucher,
-  GetAllFeedback,
-  GetUserByID,
-  GetAllRealestates,
-  CreateVouchers,
+    SignInAccount,
+    SignUpAccount,
+    GetAllAccounts,
+    DeleteAccount,
+    UpdateStatus,
+    GetAllBookings,
+    GetAllVoucher,
+    GetAllFeedback,
+    GetUserByID,
+    GetAllRealestates,
+    CreateVouchers,
+    CreateBooking,
 };
--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -4,46 +4,40 @@ import { Rating } from "@mui/material";
 import React from "react";
 
 const SearchItem = ({ searchResult }) => {
-  const BASE_URL = "http://meokool-001-site1.ltempurl.com/";
-  const [value, setValue] = React.useState(4);
+    const BASE_URL = "http://meokool-001-site1.ltempurl.com/";
+    const [value, setValue] = React.useState(4);
 
-  return (
-    <div className="search-result-content search-result-list search-result-card-list">
-      {searchResult
-        .filter((item) => item.status === "1")
-        .map((item) => {
-          const photoUrls = item.photo ? item.photo.split(",") : [];
-          return (
-            <div key={item.id} className="result-card-wrapper small-12">
-              <Link to={`/hotels/${item.id}`}>
-                <div className="result-card">
-                  <img
-                    className="result-card--photo"
-                    src={BASE_URL + photoUrls[0]}
-                    alt={item.name}
-                  />
-                  <div className="result-card--info">
-                    <h2 className="title-h2">
-                      <p>{item.name}</p>
-                    </h2>
-                    <div className="result-card--info_location">
-                      {item.location}
-                    </div>
+    return (
+        <div className="search-result-content search-result-list search-result-card-list">
+            {searchResult
+                // .filter((item) => item.status === "1")
+                .map((item) => {
+                    const photoUrls = item.photo ? item.photo.split(",") : [];
+                    return (
+                        <div key={item.id} className="result-card-wrapper small-12">
+                            <Link to={`/hotels/${item.id}`}>
+                                <div className="result-card">
+                                    <img className="result-card--photo" src={BASE_URL + photoUrls[0]} alt={item.name} />
+                                    <div className="result-card--info">
+                                        <h2 className="title-h2">
+                                            <p>{item.name}</p>
+                                        </h2>
+                                        <div className="result-card--info_location">{item.location}</div>
 
-                    <Rating name="simple-controlled" value={value} readOnly />
-                  </div>
-                  <div className="result-card--cta">
-                    <div className="result-card--cta_item">
-                      <span>{item.price}/Night</span>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-    </div>
-  );
+                                        <Rating name="simple-controlled" value={value} readOnly />
+                                    </div>
+                                    <div className="result-card--cta">
+                                        <div className="result-card--cta_item">
+                                            <span>{item.price}/Night</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 };
 
 export default SearchItem;
