800081f6a2b4a77ad5fc1fd5faadf1c0439e7f32
Tong Truong Thanh <69966374+Tom-Dev1@users.noreply.github.com>
2024-03-02 15:26:23 +0700

    css

--- src/pages/timeshare/Timeshare.jsx
+++ src/pages/timeshare/Timeshare.jsx
@@ -1,51 +1,48 @@
-import { Link, useParams } from "react-router-dom";
-import Navbar from "../../components/navbar/Navbar";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Link, useParams } from 'react-router-dom';
+import Navbar from '../../components/navbar/Navbar';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Timeshare = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchTimeshare = async () => {
-            try {
-                const response = await axios.get(
-                    `http://meokool-001-site1.ltempurl.com/api/Timeshares/GetbyRealestateID?id=${id}`,
-                );
+  useEffect(() => {
+    const fetchTimeshare = async () => {
+      try {
+        const response = await axios.get(
+          `http://meokool-001-site1.ltempurl.com/api/Timeshares/GetbyRealestateID?id=${id}`
+        );
 
-                if (response.data.data === null) {
-                    throw new Error("Network response was not ok");
-                } else {
-                    setData(response.data.data);
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchTimeshare();
-    }, []);
-
-    const handleViewBook = (timeshareID) => {};
-
-    return (
-        <div>
-            <Navbar />
+        if (response.data.data === null) {
+          throw new Error('Network response was not ok');
+        } else {
+          setData(response.data.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchTimeshare();
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {data.map((item) => (
+          <div key={item.id}>
             <div>
-                {data.map((item) => (
-                    <div key={item.id}>
-                        <div>
-                            <p>Start Day: {item.startDay}</p>
-                            <p>End Day: {item.endDay}</p>
-                        </div>
-                        <button>
-                            <Link to={`/posting/${item.id}`}>View Book</Link>
-                        </button>
-                    </div>
-                ))}
+              <p>Start Day: {item.startDay}</p>
+              <p>End Day: {item.endDay}</p>
             </div>
-        </div>
-    );
+            <button>
+              <Link to={`/posting/${item.id}`}>View Book</Link>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Timeshare;
