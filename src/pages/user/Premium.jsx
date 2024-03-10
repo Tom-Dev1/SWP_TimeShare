dd58465d8393b05fe2f9b3b42b33d6dfdb315f03
Tong Truong Thanh <69966374+Tom-Dev1@users.noreply.github.com>
2024-03-10 19:36:05 +0700

    preminum plan

--- src/pages/user/Premium.jsx
+++ src/pages/user/Premium.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import Navbar from "../../components/navbar/Navbar";
-import MailList from "../../components/mailList/MailList";
-import Footer from "../../components/footer/Footer";
-import ShowPlan from "../../components/User/Premium/ShowPLan";
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
+import ShowPlan from '../../components/User/Premium/ShowPLan';
 export default function Premium() {
   return (
     <div>
       <Navbar />
-      <ShowPlan />
+      <div className="homeContainer">
+        <ShowPlan />
+      </div>
       <MailList />
       <Footer />
     </div>
