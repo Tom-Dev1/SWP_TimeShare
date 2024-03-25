7311ee8c3bc72bebb9194d0af94d1cb58aebb377
hpdzz <tranlehuuphuoc22502@gmail.com>
2024-03-25 23:04:04 +0700

    add-banner-voucher

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import TrendPlace from "../../components/Components_LandingPages/trendPlace/TrendPlace";
 import Search from "../../components/Search/Search";
-
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import Featured from "../../components/featured/Featured";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
-
+import Banner from "../../assets/banner_SWP.svg";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
 
@@ -18,6 +18,8 @@ function Home() {
     const [isHomeTitleVisible, setIsHomeTitleVisible] = useState(false);
     const homeTitleRef = useRef(null);
     const [isHomePage] = useState(true);
+    const [isBannerVisible, setIsBannerVisible] = useState(true);
+
     useEffect(() => {
         const handleScroll = () => {
             // const currentScrollPos = window.pageYOffset;
@@ -42,9 +44,21 @@ function Home() {
         localStorage.setItem("searchkey", JSON.stringify(searchTerm));
         navigate("/hotels");
     };
-    // console.log(isHomeTitleVisible);
+    const handleOnClose = () => {
+        setIsBannerVisible(false);
+    };
     return (
         <div className={`path-frontpage ${isScrolled ? "header-scroll" : ""} ${isHomeTitleVisible ? "fixed-bs" : ""}`}>
+            {isBannerVisible && (
+                <div className="banner-voucher-container">
+                    <div className="banner_voucher-form">
+                        <img className="banner-voucher" src={Banner} alt="Voucher Banner" />
+                        <button className="cancel-btn" onClick={handleOnClose}>
+                            <HighlightOffIcon />
+                        </button>
+                    </div>
+                </div>
+            )}
             <Navbar className={isHomePage} />
             <Header />
             <Search onSearch={handleSearch} searchValue={searchValue} setSearchValue={setSearchValue} />
